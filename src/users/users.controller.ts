117bcd888841a9598094ed4c55aeae9cd68fd948
muj-i <mujahedul@sheba.xyz>
2024-12-03 08:49:15 +0600

    pass interceptors added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import {
   Body,
+  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
@@ -7,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -21,6 +23,7 @@ export class UsersController {
     return this.usersService.create(body);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/user/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
